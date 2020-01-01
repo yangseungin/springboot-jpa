e6fea4c1741e57a01326e379780e3da0e2fef2e1
seungin yang <rhfpdk92@naver.com>
2020-01-01 20:32:48 +0900

    entity class

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() { // jpa 스펙상 엔티티나 임베디드타입은 protected까지 가능
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
