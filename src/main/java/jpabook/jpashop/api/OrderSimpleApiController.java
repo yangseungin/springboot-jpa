f88a052323536deaf60069503d4ff08103cd3c0b
seungin yang <rhfpdk92@naver.com>
2020-01-08 08:27:10 +0900

    Create order search v2 (use Dto)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,16 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- *  xToOne (ManyToOne, OneToOne)
+ * xToOne (ManyToOne, OneToOne)
  * order 조회
  * Order -> Memberd
  * Order -> Delivery
@@ -22,7 +27,7 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersV1(){
+    public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName(); // Lazy 강제 초기화
@@ -30,8 +35,36 @@ public class OrderSimpleApiController {
         }
         //양방향 연관관계에서 엔티티를 직접 노출할때는 한쪽을 @JsonIgnore를 해야 무한루프에 빠지지않음
         //hibernate5Module을 사용하기보다 DTO로 반환해서 사용하는것이 좋은 방법
-
-
         return all;
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //order 2개
+        //N + 1 문제
+        //1번째쿼리발생(order) + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //lazy 초기화
+        }
+    }
+
 }
